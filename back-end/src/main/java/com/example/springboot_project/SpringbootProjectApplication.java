b652f2d632995a2151d9195492244eb599a8870c
Vetri Selvam <vetriwork1@gmail.com>
2025-02-14 20:16:52 +0530

    backend changed

---
+++ back-end/src/main/java/com/example/springboot_project/SpringbootProjectApplication.java
@@ -0,0 +1,16 @@
+package com.example.springboot_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+
+public class SpringbootProjectApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(SpringbootProjectApplication.class, args);
+		System.out.println("Hi");
+	}
+
+}
