caf375bccd744f809aba334df577edeebfba29f8
Vetri Selvam <vetriwork1@gmail.com>
2025-02-14 20:03:55 +0530

    commit two

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit here - <code>src/App.js</code> and save to reload.
+          Edit here no - <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
